4acf2a3077eaf5be9710e36a1f2b1171493e131c
Luis Amigo <lamigo@praderas.org>
2018-03-27 10:29:58 +0200

    Templated form

--- Classes/Ficheros.php
+++ Classes/Ficheros.php
@@ -25,29 +25,32 @@ class Ficheros
      */
     function post($id)
     {
-        if (isset($_POST['user']) && isset($_POST['pass'])) {
-        $user = $_POST['user'];
-        $pass = $_POST['pass'];
-        if ($user == 'javi' && $pass == 'admin') {
-                return "Prueba ficheros POST  ID de prueba {$id}";
+        if (isset($_POST['user'])) {
+            $user = $_POST['user'];
+            $pass = $_POST['pass'];
+            //It is checked if the entered username and password agree with the database
+            $RedmineClient = new LibRedmine($user, $pass);
+            if ($RedmineClient->TestUser()) {
+                return true;
             } else {
-                $message = "Wrong credentials";
                 header("HTTP/1.1 401 Unauthorized");
+                $message = "Wrong credentials";
                 $tpl = new Template;
                 $tpl->load("login.tpl");
-                $tpl->assign("message",$message);
+                $tpl->assign("message", $message);
                 $tpl->render();
                 exit;
             }
         } else {
-            header("HTTP/1.1 401 Unauthorized");
-            $id='vacio';
-            $message = "Credentials must be given";
-            $tpl = new Template;
-            $tpl->assign("message",$message);
-            $tpl->load("login.tpl");
-            $tpl->render();
-            exit;
+            header('WWW-Authenticate: Basic realm="' . self::REALM . '" ');
+            return "Prueba ficheros {$id}";
         }
+        header("HTTP/1.1 401 Unauthorized");
+        $message = "Authentication required";
+        $tpl = new Template;
+        $tpl->load("login.tpl");
+        $tpl->assign("message", $message);
+        $tpl->render();
+        exit;
     }
 }
